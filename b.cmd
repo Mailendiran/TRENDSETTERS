b58f5448aa3b28164f0f8ab920b84fe9780a4738
Myl <mylsmart2@gmail.com>
2018-11-24 19:56:09 +0530

    'Cool'

--- b.cmd
+++ b.cmd
@@ -1,8 +1,9 @@
-
+@@echo off
   git init
 git add .
 git commit -m 'Cool'
-git push -u url
+git remote add origin https://github.com/Mailendiran/TRENDSETTERS.git
+git push -u origin master
 git push heroku master
 heroku ps:scale web=1
 echo %date%,%time%
